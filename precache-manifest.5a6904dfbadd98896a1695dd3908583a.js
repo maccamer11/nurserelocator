73ddd021c43c3c45202ef75a5759ed636b235e1c
Das <maccameron@live.ca>
2020-05-02 01:16:22 -0600

    Updates

---
+++ precache-manifest.5a6904dfbadd98896a1695dd3908583a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09450eaf7659164d3f8081f3867d5864",
+    "url": "/nursing-relocater/index.html"
+  },
+  {
+    "revision": "8ecaf6b4f935977467f5",
+    "url": "/nursing-relocater/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3899ecb28fe09e416002",
+    "url": "/nursing-relocater/static/css/main.06926e44.chunk.css"
+  },
+  {
+    "revision": "8ecaf6b4f935977467f5",
+    "url": "/nursing-relocater/static/js/2.9f48ae7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nursing-relocater/static/js/2.9f48ae7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3899ecb28fe09e416002",
+    "url": "/nursing-relocater/static/js/main.4c43fcb6.chunk.js"
+  },
+  {
+    "revision": "1952ecdfb4b74c8d908b",
+    "url": "/nursing-relocater/static/js/runtime-main.0e51255c.js"
+  }
+]);
